e8ae32455abe181f3e2e9e70d789a9bce9e553e3
JeongHeon <kdrlawjdgjs@gmail.com>
2025-03-02 19:52:56 +0900

    style: tailwind color 추가 및 기타 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundColor: {
+        green: "#9DF830",
+      },
     },
   },
   plugins: [],
