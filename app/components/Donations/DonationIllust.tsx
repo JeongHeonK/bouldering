dad270a493064cacd7d452cf0af755ffbf41fa94
JeongHeon <kdrlawjdgjs@gmail.com>
2025-03-02 22:35:19 +0900

    feat: DonationIllust 분리

---
+++ app/components/Donations/DonationIllust.tsx
@@ -0,0 +1,25 @@
+export default function DonationIllust() {
+  return (
+    <div className="h-[251px] overflow-hidden">
+      <svg
+        className="relative"
+        width="1808"
+        height="430"
+        viewBox="0 0 1808 430"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <ellipse cx="904" cy="290" rx="904" ry="250" fill="#9DF830" />
+        <text
+          x="50%"
+          y="37%"
+          textAnchor="middle"
+          dy=".3em"
+          fill="black"
+          fontSize="48px"
+        >
+          Huge thanks <tspan fontWeight="bold">for your support! 🎉</tspan>
+        </text>
+      </svg>
+    </div>
+  );
+}
