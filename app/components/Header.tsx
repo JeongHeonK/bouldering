818feec0ec86965f446b57784d3ec7a89d32e8e6
JeongHeon <kdrlawjdgjs@gmail.com>
2025-03-02 19:55:06 +0900

    feat: header 생성

---
+++ app/components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import LocationDropdown from "./LocationDropdown";
+
+export default function Header() {
+  return (
+    <header className="m-0 h-[130px]">
+      <div className="fixed inset-x-10 top-3 flex items-center rounded-full bg-zinc-200/10 px-4 py-3">
+        <Image
+          src="/assets/logo-with-text.png"
+          width={308}
+          height={53}
+          alt="bouldering club"
+        />
+        <div className="flex-1" />
+        <LocationDropdown />
+        <button
+          type="button"
+          className="bg-green ml-8 rounded-full px-7 py-2.5 font-bold"
+        >
+          Donate
+        </button>
+      </div>
+    </header>
+  );
+}
