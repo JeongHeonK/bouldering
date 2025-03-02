34a088e91eee43b4a4d2bfeb3fb3b288bc03f56f
JeongHeon <kdrlawjdgjs@gmail.com>
2025-03-02 20:14:48 +0900

    style: text color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        secondary: "#6B7280",
       },
       backgroundColor: {
         green: "#9DF830",
