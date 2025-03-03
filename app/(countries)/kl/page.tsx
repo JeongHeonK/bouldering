f81c6a2f9a51200c324da300ddbf6c76d8f802e5
JeongHeon <kdrlawjdgjs@gmail.com>
2025-03-03 16:57:10 +0900

    feat: kl 페이지 완성

--- app/(countries)/kl/page.tsx
+++ app/(countries)/kl/page.tsx
@@ -1,7 +1,127 @@
+"use client";
+
+import Image from "next/image";
+import Arrow from "@/public/icon/arrow.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
 export default function Page() {
+  const handleClick = () => {
+    window.open(
+      "https://www.meetup.com/seoulboulderingclub/",
+      "_blank",
+      "noopener,noreferrer",
+    );
+  };
+
   return (
     <>
-      <main>kl</main>
+      <section className="bg-kl flex h-[568px] w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat lg:h-[940px]">
+        <h2 className="mb-2 text-7xl font-semibold text-white">
+          KL Bouldering Club
+        </h2>
+        <p className="mb-12 text-4xl text-white">Every Saturday at 3pm</p>
+        <button
+          className="bg-green rounded-full px-7 py-2.5 text-lg font-bold"
+          onClick={handleClick}
+        >
+          Join Bouldering Now
+        </button>
+      </section>
+      <section className="flex w-full flex-col items-center justify-center bg-white py-32">
+        <h2 className="text-center text-5xl">
+          Want to learn more
+          <br />
+          <span className="font-semibold">about our group?</span>
+        </h2>
+        <p className="text-secondary my-10 text-3xl">
+          Check out our Meetup app page below! 😊
+        </p>
+        <button
+          className="bg-green rounded-full px-7 py-2.5 text-lg font-bold"
+          onClick={handleClick}
+        >
+          <div className="flex flex-nowrap items-center gap-3">
+            <p>Go To Meetup</p>
+            <Arrow />
+          </div>
+        </button>
+      </section>
+      <section className="bg-lightGray pb-28 pt-20">
+        <h2 className="text-center text-5xl font-semibold">Our Memories</h2>
+        <p className="text-secondary my-4 pb-10 text-center text-3xl">
+          Work out, play, and have a great time together!
+        </p>
+        <Swiper
+          className="mySwiper"
+          modules={[Autoplay]}
+          spaceBetween={50}
+          slidesPerView={3}
+          loop
+          speed={4000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <Image
+              src="/kl/kl1.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/kl/kl2.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/kl/kl3.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/kl/kl4.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/kl/kl5.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src="/kl/kl6.png"
+              alt="seoul1"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </section>
     </>
   );
 }
